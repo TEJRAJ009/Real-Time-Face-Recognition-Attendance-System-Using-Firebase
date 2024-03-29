313eb2e01da72bb2770002a1ed7d32f91eea7062
Tejraj Jadhav <tejrajjadhavj7@gmail.com>
2024-03-29 19:49:09 +0530

    Completed

---
+++ AddDatatoDatabase.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': ""
+})
+
+ref = db.reference('Students')
+
+data = {
+    "963852":
+        {
+            "name": "Elon Musk",
+            "Department": "Physics",
+            "starting_year": 1717,
+            "total_attendance": 0,
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
